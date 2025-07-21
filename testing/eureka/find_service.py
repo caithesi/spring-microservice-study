107074147d1be1e3d6116ec0b6cc55e9cbd935db
Cai The Si <caithesi@gmail.com>
2025-07-22 05:44:14 +0700

    simplify testing py with eureka

--- testing/eureka/find_service.py
+++ testing/eureka/find_service.py
@@ -3,11 +3,14 @@ import json
 
 import sys
 import os
-
+import argparse
 from pathlib import Path
 from variables import load_variables
 from make_request import format_url, test
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--service', required=True)
+args = parser.parse_args()
 
 
 # get path to current file's directory
@@ -17,11 +20,12 @@ this_dir = Path(__file__).parent
 resource_path = this_dir / "variables.json"
 
 variables = load_variables(resource_path,
-                           local_variables = {
+                            local_variables = {
+                             "service":args.service
                            })
 
 print(variables)
-x = requests.get(format_url("{localhost}/licensing-service", variables),
-                 headers={"X-Config-Token":"myroot", "Content-Type":"application/json"})
+x = requests.get(format_url("{localhost}/{service}", variables),
+                  headers={"X-Config-Token":"myroot", "Content-Type":"application/json"})
 print(x.text)
 
